4c6d887bf4445595cb67f0475aea96c4f31b0a2e
Kevin O'Brien <kpobrien@buffalo.edu>
2020-01-03 15:46:01 -0500

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Kevin O'Brien on 1/3/20.
+//  Copyright © 2020 Kevin O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
